95e8390522199d346f039761a84fad9bddc4e7ae
Garfeild <garfeild.ubuntu@gmail.com>
2009-04-10 23:10:21 +0400

    Added table.* with TreeView table template

---
+++ table.c
@@ -0,0 +1,59 @@
+#include "table.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+ 
+void set_column(GtkWidget *tree, const char *labelColumn[])
+{
+  GtkTreeViewColumn *column;
+  GtkCellRenderer *renderer;
+  int i;
+ 
+  for ( i=0; i < N_COLUMNS; i++ )
+  {
+ 
+      if ( i == 2 )
+      {
+          renderer = gtk_cell_renderer_toggle_new ();
+          column = gtk_tree_view_column_new_with_attributes (labelColumn[i], renderer,
+                  "active", i,
+                  NULL);
+      }
+      else
+      {
+          renderer = gtk_cell_renderer_text_new ();
+          column = gtk_tree_view_column_new_with_attributes (labelColumn[i], renderer,
+                  "text", i,
+                  NULL);
+      }
+ 
+      gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
+  }
+}
+ 
+void set_table_info(GtkTreeStore *store, const char *names[], gboolean flag)
+{
+    GtkTreeIter iter;
+ 
+    gtk_tree_store_append (store, &iter, NULL); /* Acquire an iterator */
+    gtk_tree_store_set (store, &iter,
+            TITLE_COLUMN, names[TITLE_COLUMN],
+            AUTHOR_COLUMN, names[AUTHOR_COLUMN],
+            CHECKED_COLUMN, flag,
+            -1);
+}
+ 
+GtkWidget* setup_table(GtkTreeStore *store, const char *labelColumn[])
+{
+  GtkWidget *tree;
+ 
+  tree = gtk_tree_view_new_with_model (GTK_TREE_MODEL (store));
+ 
+  set_column(tree, labelColumn);
+ 
+  g_object_unref (G_OBJECT (store));
+ 
+  g_print("setup\n");
+  return tree;
+ 
+}
---
+++ table.h
@@ -0,0 +1,18 @@
+#ifndef TABLE_H
+#define TABLE_H
+ 
+enum
+{
+    TITLE_COLUMN,
+    AUTHOR_COLUMN,
+    CHECKED_COLUMN,
+    N_COLUMNS
+};
+ 
+void set_column(GtkWidget *tree, char *labelColumn[]);
+ 
+void set_table_info(GtkTreeStore *store,const char *names[], gboolean flag);
+ 
+GtkWidget* setup_table(GtkTreeStore *store, const char *labelColumn[]);
+ 
+#endif 
