220133dee2f268b4f4ca8597bc742317597896f4
xushaw <xubunta@gmail.com>
2009-05-08 04:05:35 +0800

    уже 8-ое
    Signed-off-by: Garfeild <garfeild.ubuntu@gmail.com>

--- main.c
+++ main.c
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
   for(i=0; i<2; i++)  {
     label = gtk_label_new(nameLabel[i]);
     scr_window = gtk_scrolled_window_new (NULL, NULL);
-    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scr_window), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scr_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     //Твой таб.
     if (i == 0)
         table = tab1();
